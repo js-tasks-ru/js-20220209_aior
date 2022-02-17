a209fbd4214f3f38e639c448acddc199607cfc7f
Den Degtiarev <aior@mail.ru>
2022-02-17 09:51:58 +0300

    solution tast "2-invert-object"

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj) {
+    let map = new Map();
+    for (let key in obj) {
+      map.set(obj[key], key); // John, 30, true
+    }
+    return Object.fromEntries(map.entries());
+  } else {
+    return undefined;
+  }
 }
