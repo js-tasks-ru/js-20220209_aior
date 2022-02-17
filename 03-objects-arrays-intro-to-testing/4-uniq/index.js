9902f67fea73bb9f4917046118fa074da2c8dafe
Den Degtiarev <aior@mail.ru>
2022-02-17 10:01:14 +0300

    solution task "4-uniq"

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,15 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (arr) {
+    let set = new Set();
 
+    for (let key of arr) {
+      set.add(key);
+    }
+
+    return Array.from(set);
+  } else {
+    return [];
+  }
 }
